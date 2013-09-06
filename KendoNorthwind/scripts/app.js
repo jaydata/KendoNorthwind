e4be94616c8237367eec9e6ca1baa5819253c047
Viktor Lázár <lazarv1982@gmail.com>
2013-09-06 01:45:21 -0500

    Changes made by Viktor Lázár (lazarv1982@gmail.com).

--- KendoNorthwind/scripts/app.js
+++ KendoNorthwind/scripts/app.js
@@ -3,19 +3,32 @@
         app = global.app = global.app || {};
 
     document.addEventListener("deviceready", function () {
-        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
+        $data.initService("http://jaydata.org/examples/Northwind.svc").then(function(db, factory, type){
+            app.database = db;
+            app.application = new kendo.mobile.Application(document.body, {
+                layout: "tabstrip-layout",
+                transition: "slide"
+            });
+        });
     }, false);
 
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
+    app.changeSkin = function () {
+        app.application.skin(this.selectedIndex == 0 ? "" : "flat");
+    };
+    
+    app.showProducts = function(e){
+        e.view.scroller.reset();
+        $("#products-listview").data("kendoMobileListView").setDataSource(
+            app.database.Products.include("Supplier").filter(function(it){
+                return it.Category_ID == this.id;
+            }, {
+                id: e.view.params.categoryid
+            }).asKendoDataSource()
+        );
+    };
+    
+    app.showProductDetails = function(e){
+        $(".product-details.active").removeClass("active");
+        $(e.button.context).next(".product-details").addClass("active");
     };
 })(window)
